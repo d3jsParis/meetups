52ca824ba7ae8a2e4761557560fdeaf0765ae582
Charles Miglietti <charles.miglietti@yahoo.fr>
2014-07-08 11:58:09 +0200

    meetup#1 add some data

--- meetup 08-07-2014/e2e-example/scripts/mapchart.js
+++ meetup 08-07-2014/e2e-example/scripts/mapchart.js
@@ -3,7 +3,7 @@ window.France = (function() {
 
   function Constructor (params) {
     //useful data
-    //this.data = _.clone(params.data);
+    this.data = _.clone(params.data);
     this.geoData = params.geoData;
     this.parent = params.parent;
     this.colors = params.colors;
@@ -20,21 +20,32 @@ window.France = (function() {
   Constructor.prototype.init = function () {
     var self = this;
 
+
+		// CREATE SVG CONTAINER
     this.container = this.parent.append('g')
       .attr('transform', 'matrix('+this.factor+',0,0,'+this.factor+',-110,-80)')
       .append('g');
 
+		// CREATE SCALE
+    this.quantile = d3.scale.quantile()
+      .domain(_.values(_.pick(this.data[this.data.length - 1],this.group)))
+      .range(d3.range(this.colors.length));
+
+    this.cValue = this.data.length - 1;
+
+		// FILL AREA WITH CORRESPONDING COLOR BASED ON DATA
     this.container.selectAll('.region')
        .data(this.geoData.regions)
        .enter().append('path')
        .attr('class', 'region')
        .attr('id', function (d) {return d.name;})
-       .attr('fill', function (d,i) {return 'rgb('+4*i +','+(250-3*i)+','+(100+2*i)+')';})
+       .attr('fill', function (d) {return self.colors[self.quantile(self.data[self.cValue][d.name])];})
        .attr('d', function (d) {return d.path.split(' C')[0] ;})
     return this;
   };
 
   Constructor.prototype.show = function () {
+		// TRANSITION + DELAY POUR AFFICHAGE
     this.container.selectAll('.region')
       .transition()
       .delay(function(d,i) {return 30*i;})
--- meetup 08-07-2014/e2e-example/scripts/regions.js
+++ meetup 08-07-2014/e2e-example/scripts/regions.js
@@ -5,6 +5,8 @@ var centered
   , margin = {top: 40, right: 120, bottom: 30, left: 50}
   , width = 690 - margin.left - margin.right
   , height = 600 - margin.top - margin.bottom
+  , factor = 0.5
+  , mapColors = ['#FCC5C0','#FA9FB5','#F768A1', '#7A0177']
   , svg;
 
 
@@ -13,17 +15,33 @@ svg = d3.select('.map').append('svg')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom);
 
+// CHARGEMENT DU FOND DE CARTE
 d3.json('data/regions.json', function(france) {
+// CHARGEMENT DES DONNEES DE CARTO
+  d3.csv('data/regions-data.csv', function (data) {
+// FORMATTAGE DES DONNEES
+    var entries = d3.keys(data[0]).filter(function(key) { return key !== 'date'; });
+    data.forEach(function (d) {
+      entries.forEach(function(e) {
+			// PARSE FLOAT
+        d[e] = +d[e];
+      });
+    });
+
     var map = new France({ width: width
                          , height: height
                          , parent: svg
+                         , colors: mapColors
+                         , group: entries
                          , geoData: france
-                         })
+                         , data: data})
     map.init()
       .show()
+      .addLegend();
 
 
   });
+});
 
 
 
